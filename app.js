88643f4eef576523a4972af7455609559bb6f536
moiz <amkhatri89@gmail.com>
2023-11-14 18:35:41 +0500

    right character
    cant go left and right

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ var left = 0;
 
 function abc() {
     var character = document.getElementById("character");
-    if (event.keyCode === 68) {
-        left = left + 10;
-        character.style.left = left + "px";
-    }
-    if (event.keyCode === 68 && left < 1000) {
+    if (event.keyCode === 68 && left < 1200) {
         left = left + 10;
         character.style.left = left + "px";
     }
@@ -25,12 +21,62 @@ function abc() {
     if (event.keyCode === 83) {
         character.src = 'images/spidey-crouch.gif'
         // character.style.width = "400px"
-        // character.style.height = "230px"
+        character.style.height = "230px"
         setTimeout(function(){
             character.src = "images/m-spiderman.gif"
         }, 200)
     }
-    console.log(left)
-    
+
+    var character2 = document.getElementById("character2");
+    if (event.keyCode === 37 && left > 1200) {
+        left = left - 10;
+        character2.style.right = left + "px";
+        character2.src = 'images/iron-walk.gif'
+        character2.style.width = "400px"
+        character2.style.height = "230px"
+        setTimeout(function(){
+            character2.src = "images/iron-man-stance.gif"
+        }, 3000)
+    }
+    if (event.keyCode === 39 && left > 60) {
+        left = left + 10;
+        character2.style.right = left + "px";
+        character2.src = 'images/iron-walk.gif'
+        character2.style.width = "400px"
+        character2.style.height = "230px"
+        setTimeout(function(){
+            character2.src = "images/iron-man-stance.gif"
+        }, 3000)
+    }
+    if (event.keyCode === 38) {
+        character2.src = 'images/iron-fly.gif'
+        character2.style.width = "400px"
+        character2.style.height = "400px"
+        setTimeout(function(){
+            character2.src = "images/iron-man-stance.gif"
+            character2.style.height = "230px"
+        }, 1000)
+    }
+    if (event.keyCode === 40) {
+        character2.src = 'images/iron-chk.gif'
+        character.style.width = "400px"
+        character.style.height = "230px"
+        setTimeout(function(){
+            character2.src = "images/iron-man-stance.gif"
+            character2.style.height = "230px"
+        }, 1000)
+    }
+
+ console.log(left)    
 }
+
+
+//////character2////
+
+
+function bcd(){
+    
+   
+}
+window.onkeydown = bcd;
 window.onkeydown = abc;
